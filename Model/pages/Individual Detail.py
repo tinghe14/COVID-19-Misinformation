dcec1ad698a6dd0b4e482bea89d9edba69949c6b
Ting He <hetinghelen@gmail.com>
2022-08-14 14:17:27 -0400

    upload models

---
+++ Model/pages/Individual Detail.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('COVID Misleading Informaiton Detection')
+st.header("Individual Detail 📑")
+st.sidebar.markdown("Individual Detail 📑")
